5b7723889a25dcd349a6982c15262616038f7c67
Lana Gomes <lana.gomes1919@gmail.com>
2024-05-08 14:53:15 -0300

    creating colors configuration tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,react}"],
   theme: {
+    colors: {
+      magenta: "#DA6E7B",
+      blue: "#708FDF",
+      purple: "#7B35B1",
+      white: "#EFEFEF",
+      grey: {
+        1: "#C2C2C2",
+        2: "#7C7C7C",
+        3: "#3C3C3C",
+        4: "#121212",
+      },
+    },
     extend: {},
   },
   plugins: [],
