b9e0f6e7fa42abf1e99fc9684b725d888903b69e
Lana Gomes <lana.gomes1919@gmail.com>
2024-05-08 14:51:38 -0300

    adding Icon in NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,10 @@
+import IconMenu64 from "../Images/menuIcon64.png";
+
 function NavBar() {
-  return <div className="bg-red-300">NavBar</div>;
+  return (
+    <nav className="bg-white flex justify-end items-center">
+      <img className="h-14 m-1.5" src={IconMenu64} />
+    </nav>
+  );
 }
 export default NavBar;
