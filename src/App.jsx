b82ab03332354b0c7ac6e363808667e724cc7ec9
Lana Gomes <lana.gomes1919@gmail.com>
2024-05-08 12:07:15 -0300

    including the separated components in App

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+import "./App.css";
+import NavBar from "./components/NavBar";
+import MainMessage from "./components/MainMessage";
+import StopWatch from "./components/StopWatch";
+import Buttons from "./components/Buttons";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <MainMessage />
+      <StopWatch />
+      <Buttons />
+    </>
+  );
+}
+
+export default App;
