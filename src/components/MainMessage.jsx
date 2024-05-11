582d839263da25b1acd2d28aea66550238c4303e
Lana Gomes <lana.gomes1919@gmail.com>
2024-05-11 13:43:01 -0300

    adding margin and padding in responsive layout - MainMessage

--- src/components/MainMessage.jsx
+++ src/components/MainMessage.jsx
@@ -4,7 +4,7 @@ function MainMessage() {
   const [message, setMessage] = useState(true);
   return (
     <div>
-      <main className="bg-grey-1 my-4 p-4 flex-1 text-center sm:p-8 lg:p-12">
+      <main className="bg-grey-1 my-10 p-8 text-xl flex-1 text-center sm:p-10 sm:text-2xl sm:my-6 lg:text-3xl lg:p-12">
         <h2 className="font-bold">Hora de Relaxar!</h2>
         <p>Você tem 1 hora para descansar</p>
       </main>
