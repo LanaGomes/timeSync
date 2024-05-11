8f84c6b95fbb733ffb803e569a183dc016d8a7ef
Lana Gomes <lana.gomes1919@gmail.com>
2024-05-11 11:44:43 -0300

    adding design to MainMessage

--- src/components/MainMessage.jsx
+++ src/components/MainMessage.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 function MainMessage() {
-  return <div>MainMessage</div>;
+  const [message, setMessage] = useState(true);
+  return (
+    <div>
+      <main className="bg-grey-1 my-4 p-4 flex-1 text-center sm:p-8 lg:p-12">
+        <h2 className="font-bold">Hora de Relaxar!</h2>
+        <p>Você tem 1 hora para descansar</p>
+      </main>
+    </div>
+  );
 }
 export default MainMessage;
