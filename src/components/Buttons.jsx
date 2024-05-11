66de4af5c1b878c0d1ff54324f5f2886303786fc
Lana Gomes <lana.gomes1919@gmail.com>
2024-05-11 13:43:36 -0300

    Defining gradient colors - Buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,4 +1,13 @@
 function Buttons() {
-  return <div>Buttons</div>;
+  return (
+    <div className="flex place-content-around ms:px-40 md:px-20  lg:px-40 xl:px-96 mt-8 lg:m-4 text-2xl md:text-3xl text-white font-bold ">
+      <button className="bg-gradient-to-t from-darkBlue to-blue px-10 py-12 sm:px-20 rounded-lg md:mr-8 ">
+        Relaxar
+      </button>
+      <button className="bg-gradient-to-b from-magenta to-red px-10 py-12 sm:px-20 rounded-lg ">
+        Focar
+      </button>
+    </div>
+  );
 }
 export default Buttons;
