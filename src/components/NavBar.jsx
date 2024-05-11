d507e741cb44191bffe102848eec7ca2f2b14ca5
Lana Gomes <lana.gomes1919@gmail.com>
2024-05-11 11:44:14 -0300

    fixing the alighment

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import IconMenu64 from "../Images/menuIcon64.png";
 
 function NavBar() {
   return (
-    <nav className="bg-white flex justify-end items-center">
+    <nav className="bg-white flex justify-end items-center ">
       <img className="h-14 m-1.5" src={IconMenu64} />
     </nav>
   );
