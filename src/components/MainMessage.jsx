ada7b12bc5ef3a05661dbff27161fa1b79fab6b7
Lana Gomes <lana.gomes1919@gmail.com>
2024-06-17 08:46:55 -0300

    fixing responsiveness

--- src/components/MainMessage.jsx
+++ src/components/MainMessage.jsx
@@ -4,7 +4,7 @@ function MainMessage() {
   const [message, setMessage] = useState(true);
   return (
     <div>
-      <main className="bg-grey-1  p-8 text-xl flex-1 text-center sm:p-10 sm:text-2xl lg:text-2xl lg:p-8">
+      <main className="bg-grey-1  text-xl flex-1 text-center sm:p-4 sm:text-1xl lg:text-2xl lg:p-4">
         <h2 className="font-bold">Hora de Relaxar!</h2>
         <p>Você tem 1 hora para descansar</p>
       </main>
