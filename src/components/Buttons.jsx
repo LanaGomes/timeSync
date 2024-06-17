577e075cd0e05a68234c1d947ebf81f9b2e5544b
Lana Gomes <lana.gomes1919@gmail.com>
2024-06-17 08:47:13 -0300

    fixing responsiveness

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -8,7 +8,7 @@ function Buttons({
     <div className="flex place-content-around  lg:px-40 xl:px-56 xl:mt-10 mt-8 lg:m-4 text-2xl md:text-3xl text-white font-bold">
       <button
         onClick={restTimeClickHandler}
-        className=" whitespace-nowrap text-center bg-gradient-to-t from-darkBlue to-blue px-10 py-10 mb-3 sm:px-20 rounded-lg md:mr-8 drop-shadow-lg"
+        className=" whitespace-nowrap text-center bg-gradient-to-t from-darkBlue to-blue  py-8 mb-3 sm:px-20 rounded-lg md:mr-8 drop-shadow-lg"
       >
         {mode === "rest" && itsRunning ? "PARAR" : "Relaxar"}
       </button>
