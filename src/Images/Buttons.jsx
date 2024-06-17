305536727a6bf066fd42aba7cb62a2793e12e424
Lana Gomes <lana.gomes1919@gmail.com>
2024-06-17 07:12:42 -0300

    adding logic to Buttons

---
+++ src/Images/Buttons.jsx
@@ -0,0 +1,25 @@
+function Buttons({
+  focusTimeClickHandler,
+  restTimeClickHandler,
+  mode,
+  itsRunning,
+}) {
+  return (
+    <div className="flex place-content-around ms:px-40 md:px-20 lg:px-40 xl:px-56 xl:mt-10 mt-8 lg:m-4 text-2xl md:text-3xl text-white font-bold">
+      <button
+        onClick={restTimeClickHandler}
+        className="w-36 whitespace-nowrap text-center bg-gradient-to-t from-darkBlue to-blue px-10 py-10 mb-3 sm:px-20 rounded-lg md:mr-8 drop-shadow-lg"
+      >
+        {mode === "rest" && itsRunning ? "PARAR" : "Relaxar"}
+      </button>
+      <button
+        onClick={focusTimeClickHandler}
+        className="w-36 whitespace-nowrap text-center bg-gradient-to-b from-magenta to-red px-10 py-10 mb-3 sm:px-20 rounded-lg drop-shadow-lg"
+      >
+        {mode === "focus" && itsRunning ? "PARAR" : "Focar"}
+      </button>
+    </div>
+  );
+}
+
+export default Buttons;
