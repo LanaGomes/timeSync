c3751807881757eeee2a6c21fb0c3a7371d213fc
Lana Gomes <lana.gomes1919@gmail.com>
2024-06-17 07:14:46 -0300

    updating components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
 
 import "./App.css";
-import NavBar from "./components/NavBar";
+
 import MainMessage from "./components/MainMessage";
 import StopWatch from "./components/StopWatch";
-import Buttons from "./components/Buttons";
 
 function App() {
   return (
     <div className="bg-reallywhite  ">
-      <NavBar />
       <MainMessage />
       <StopWatch />
-      <Buttons />
+
       <div className=""></div>
     </div>
   );
