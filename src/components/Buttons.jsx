3c81da31b54af164b1816560fde353f22f0fffab
Lana Gomes <lana.gomes1919@gmail.com>
2024-06-17 07:14:27 -0300

    transfering logic from Stopwatch to Buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,13 +1,25 @@
-function Buttons() {
+function Buttons({
+  focusTimeClickHandler,
+  restTimeClickHandler,
+  mode,
+  itsRunning,
+}) {
   return (
-    <div className="flex place-content-around ms:px-40 md:px-20  lg:px-40 xl:px-56 xl:mt-10 mt-8 lg:m-4 text-2xl md:text-3xl text-white font-bold ">
-      <button className="bg-gradient-to-t from-darkBlue to-blue px-10 py-10 mb-3 sm:px-20 rounded-lg md:mr-8 drop-shadow-lg ">
-        Relaxar
+    <div className="flex place-content-around  lg:px-40 xl:px-56 xl:mt-10 mt-8 lg:m-4 text-2xl md:text-3xl text-white font-bold">
+      <button
+        onClick={restTimeClickHandler}
+        className=" whitespace-nowrap text-center bg-gradient-to-t from-darkBlue to-blue px-10 py-10 mb-3 sm:px-20 rounded-lg md:mr-8 drop-shadow-lg"
+      >
+        {mode === "rest" && itsRunning ? "PARAR" : "Relaxar"}
       </button>
-      <button className="bg-gradient-to-b from-magenta to-red px-10 py-10 mb-3 sm:px-20 rounded-lg drop-shadow-lg ">
-        Focar
+      <button
+        onClick={focusTimeClickHandler}
+        className=" whitespace-nowrap text-center bg-gradient-to-b from-magenta to-red px-4 py-4 mb-3 sm:px-20 rounded-lg drop-shadow-lg"
+      >
+        {mode === "focus" && itsRunning ? "PARAR" : "Focar"}
       </button>
     </div>
   );
 }
+
 export default Buttons;
