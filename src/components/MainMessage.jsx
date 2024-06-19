2ec38e60411375c9b6dfe853adace933f0705b22
Lana Gomes <lana.gomes1919@gmail.com>
2024-06-19 09:32:51 -0300

    perzonalizing MainMessage

--- src/components/MainMessage.jsx
+++ src/components/MainMessage.jsx
@@ -1,22 +1,50 @@
 import { useState } from "react";
 
-function MainMessage({ mode }) {
+function MainMessage({ mode, sign, time, hours, minutes, seconds }) {
   const getClassNameMainMessage = () => {
     if (mode === "rest") {
-      return "bg-blue p-6 text-xl flex-1 text-center  sm:p-4 sm:text-1xl lg:text-3xl lg:p-8 text-white";
+      return "bg-blue p-6 text-xl flex-1 text-center sm:text-1xl lg:text-3xl lg:p-8 text-white";
     } else if (mode === "focus") {
-      return "bg-red p-6 text-xl flex-1 text-center sm:p-4 sm:text-1xl lg:text-3xl lg:p-8  text-white";
+      return "bg-red p-6 text-xl flex-1 text-center sm:text-1xl lg:text-3xl lg:p-8  text-white";
     } else {
-      return "bg-purple p-6 text-xl flex-1 text-center sm:p-4 sm:text-1xl lg:text-3xl lg:p-8  text-white";
+      return "bg-purple p-6 text-xl flex-1 text-center sm:text-1xl lg:text-3xl lg:p-8  text-white";
+    }
+  };
+  const timeBalance = () => {
+    if (sign == " ") {
+      return (
+        <p className="pt-4 text-xl">{`Você precisa DESCANSAR esse tempo : ${hours
+          .toString()
+          .padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
+          .toString()
+          .padStart(2, "0")}`}</p>
+      );
+    } else if (sign == "-") {
+      return (
+        <p className="pt-4 text-xl">{`Você precisa FOCAR esse tempo : ${hours
+          .toString()
+          .padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
+          .toString()
+          .padStart(2, "0")}`}</p>
+      );
     }
   };
   return (
     <div>
+      {" "}
       <main className={getClassNameMainMessage()}>
         <h2 className="px-10 font-semibold">
           {mode == "rest" ? "Hora de Relaxar" : "Vamos focar!"}
         </h2>
-        <p>{}</p>
+
+        {time == 0 ? (
+          <p className="pt-4 text-xl">
+            "O primeiro passo é sempre o mais difícil, mas não deixa de ser só
+            um passo"
+          </p>
+        ) : (
+          timeBalance()
+        )}
       </main>
     </div>
   );
