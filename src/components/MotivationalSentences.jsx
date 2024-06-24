4d02b8d60812180f3d34e97cbbf070cc372fd236
Lana Gomes <lana.gomes1919@gmail.com>
2024-06-24 08:45:00 -0300

    changing error text to quote error

--- src/components/MotivationalSentences.jsx
+++ src/components/MotivationalSentences.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import errorImg from "../Images/icons8-erro.gif";
+import loadingImg from "../Images/loadingImg.gif";
 
 const MotivationalSentences = () => {
   const [quote, setQuote] = useState("");
@@ -23,7 +25,9 @@ const MotivationalSentences = () => {
         const response = await fetch(url, options);
 
         if (!response.ok) {
-          throw new Error("Erro na requisição");
+          throw new Error(
+            '"Cada erro em sua requisição é uma oportunidade de aprendizado e crescimento. Persevere e transforme obstáculos em degraus para o sucesso"'
+          );
         }
 
         const result = await response.json();
@@ -41,16 +45,32 @@ const MotivationalSentences = () => {
   }, []);
 
   if (loading) {
-    return <p>Carregando...</p>;
+    return (
+      <div>
+        <img src={loadingImg}></img>
+        <p>Carregando...</p>
+      </div>
+    );
   }
 
   if (error) {
-    return <p>Erro: {error}</p>;
+    return (
+      <div className="flex justify-center items-center my-6 mx-12 text-xs sm:text-base md:my-10 md:mx-36 gap-5 italic">
+        {" "}
+        <img
+          className=" flex justify-center items-center h-10 w-10"
+          src={errorImg}
+        ></img>{" "}
+        Erro: {error}
+      </div>
+    );
   }
 
   return (
-    <div>
-      <p>{quote}</p>
+    <div className="flex my-10 mx-36 gap-5 italic">
+      <blockquote>
+        <q>{quote}</q>{" "}
+      </blockquote>
       <p>{author}</p>
     </div>
   );
