e557e93671df5427c0558ef0fdbb73d04a9ccd08
Lana Gomes <lana.gomes1919@gmail.com>
2024-06-26 17:04:36 -0300

    creating a bank of sentences

--- src/components/MotivationalSentences.jsx
+++ src/components/MotivationalSentences.jsx
@@ -1,55 +1,29 @@
 import React, { useState, useEffect } from "react";
-import errorImg from "./../../public/Images/icons8-erro.gif";
+import BancoFrasesMotivacionais from "./BancoFrasesMotivacionais";
 import loadingImg from "./../../public/Images/loadingImg.gif";
 
 const MotivationalSentences = () => {
   const [quote, setQuote] = useState(null);
+  const [author, setAuthor] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
-  const fetchQuote = async () => {
-    let apiKey = process.env.RAPIDAPI_KEY;
-    const url = "https://quotes85.p.rapidapi.com/keyword?word=success";
-    const options = {
-      method: "GET",
-      headers: {
-        "x-rapidapi-key": apiKey,
-        "x-rapidapi-host": "quotes85.p.rapidapi.com",
-      },
-    };
+  const fetchRandomQuote = () => {
+    const randomIndex = Math.floor(
+      Math.random() * BancoFrasesMotivacionais.length
+    );
+    const randomQuote = BancoFrasesMotivacionais[randomIndex];
 
-    try {
-      const response = await fetch(url, options);
-
-      if (!response.ok) {
-        throw new Error(
-          '"Cada erro em sua requisição é uma oportunidade de aprendizado e crescimento. Persevere e transforme obstáculos em degraus para o sucesso"'
-        );
-      }
-
-      const result = await response.json();
-      console.log(result);
-
-      if (Array.isArray(result) && result.length > 0) {
-        const randomIndex = Math.floor(Math.random() * result.length);
-        setQuote(result[randomIndex]);
-      } else {
-        throw new Error("Nenhuma citação encontrada.");
-      }
-
-      setLoading(false);
-    } catch (error) {
-      setError(error.message);
-      setLoading(false);
-    }
+    setQuote(randomQuote.quote);
+    setAuthor(randomQuote.author);
   };
 
   useEffect(() => {
-    fetchQuote();
+    fetchRandomQuote();
+    setLoading(false);
 
     const intervalId = setInterval(() => {
-      fetchQuote(); // Fetch a new quote every hour
-    }, 3600000);
+      fetchRandomQuote();
+    }, 1800000);
 
     return () => clearInterval(intervalId);
   }, []);
@@ -62,25 +36,12 @@ const MotivationalSentences = () => {
       </div>
     );
   }
-
-  if (error) {
-    return (
-      <div className="flex justify-center items-center my-6 mx-12 text-xs text-left text-grey-2 sm:text-base md:my-10 md:mx-36 gap-5 italic">
-        <img
-          className="flex justify-center items-center h-10 w-10"
-          src={errorImg}
-          alt="Error"
-        ></img>
-        Erro: {error}
-      </div>
-    );
-  }
-
   return (
     <div className="my-6 mx-12 text-lg text-center text-grey-2 italic">
       <blockquote>
         <q>{quote}</q>
       </blockquote>
+      <p>{author}</p>
     </div>
   );
 };
