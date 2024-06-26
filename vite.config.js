b97db0ca749a35764cb61cf4b9edfa02b790b494
Lana Gomes <lana.gomes1919@gmail.com>
2024-06-25 21:46:33 -0300

    another try to use env virables in vercel '-'

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: process.env.PUBLIC_URL,
+  define: {
+    __RAPIDAPI_KEY__: process.env.VITE_RAPIDAPI_KEY,
+  },
 });
