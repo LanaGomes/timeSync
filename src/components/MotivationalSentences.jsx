f7c2c676dba265ea431630c47e55f839fc520664
Lana Gomes <lana.gomes1919@gmail.com>
2024-06-25 22:09:09 -0300

    is this it?

--- src/components/MotivationalSentences.jsx
+++ src/components/MotivationalSentences.jsx
@@ -9,15 +9,9 @@ const MotivationalSentences = () => {
 
   const fetchQuote = async () => {
     const url = "https://quotes85.p.rapidapi.com/keyword?word=focus";
-    let apiKey = "";
-    try {
-      apiKey = process.env.RAPIDAPI_KEY;
-    } catch (e) {
-      console.log(e);
-      apiKey = import.meta.env.VITE_RAPIDAPI_KEY;
-    }
-    console.log(import.meta.env.VITE_RAPIDAPI_KEY);
-    console.log("maoe");
+    let apiKey = process.env.RAPIDAPI_KEY;
+    console.log(apiKey);
+
     const options = {
       method: "GET",
       headers: {
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  console.log(env);
   return {
     plugins: [react()],
     base: env.PUBLIC_URL,
+    define: {
+      "process.env": env,
+      RAPIDAPI_KEY: process.env.RAPIDAPI_KEY,
+    },
   };
 });
